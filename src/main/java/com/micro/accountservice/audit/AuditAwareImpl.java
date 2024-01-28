2d09db826632acd940d98dbebf4d3d2e3815e49c
kushanxp <win32.sysP@88w0rd>
2024-01-28 12:14:42 -0500

    changes

--- src/main/java/com/micro/accountservice/audit/AuditAwareImpl.java
+++ src/main/java/com/micro/accountservice/audit/AuditAwareImpl.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 public class AuditAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of("Account_Microservice");
+        return Optional.of("Account_Microservice_User");
     }
 }
